0d999507ead356c6c2a2455418a702c48b03209a
Eric <ewmassey@gmail.com>
2013-08-04 19:41:18 -0700

    setting up mongohq

--- conf.js
+++ conf.js
@@ -2,8 +2,8 @@ var config = {};
 
 config.mongo_host = "localhost";
 config.mongo_db = "burner";
-config.mongo_uri = "mongodb://" + config.mongo_host + "/" + config.mongo_db;
+config.mongo_uri = process.env.MONGOHQ_URL || "mongodb://" + config.mongo_host + "/" + config.mongo_db;
 
-config.root_url = "http://localhost:5000";
+config.root_url = process.env.ROOT_URL || "http://localhost:5000";
 
 module.exports = config;
