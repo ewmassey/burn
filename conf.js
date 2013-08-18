6b17f072b1ab99d0b4a9bc7d46fde62c0cdb5c87
Eric <ewmassey@gmail.com>
2013-08-18 12:58:41 -0700

    fixed live url

--- conf.js
+++ conf.js
@@ -4,6 +4,6 @@ config.mongo_host = "localhost";
 config.mongo_db = "burner";
 config.mongo_uri = process.env.MONGOHQ_URL || "mongodb://" + config.mongo_host + "/" + config.mongo_db;
 
-config.root_url = process.env.ROOT_URL || "http://localhost:5000";
+config.root_url = "http://" + process.env.ROOT_URL || "http://localhost:5000";
 
 module.exports = config;
